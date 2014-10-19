33aea49df2c1ad288a1e142250905dc07fddc959
Travis Weston <tweston@bangordailynews.com>
2014-10-19 18:37:44 -0400

    Added a solution for Problem 7

--- Problem007/problem7.py
+++ Problem007/problem7.py
@@ -3,3 +3,24 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 '''
+from math import sqrt
+def nth_prime( n ):
+	i = 2
+	count = 0
+	while( True ):
+		sqr = sqrt(i)
+		j = 2
+		prime = True
+		while j <= sqr:
+			if i % j == 0:
+				prime = False
+			j += 1
+		if prime:
+			count += 1
+
+		if count == n:
+			return i
+
+		i += 1
+
+print nth_prime( 10001 )
