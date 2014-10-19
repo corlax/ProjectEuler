fd9ba94d061727e37cb19a68dba31b7036182df1
Travis Weston <tweston@bangordailynews.com>
2014-10-19 16:07:33 -0400

    Added a solution to Problem 5. This solution is not optimized, and could do with a better solution at a later date.

--- Problem005/problem5.py
+++ Problem005/problem5.py
@@ -3,3 +3,21 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
+def check( value, against ):
+	if( value % against > 0 ):
+		return False
+	elif( against > 1 ):
+		return check( value, against - 1 )
+	else:
+		return True
+
+value = 2520
+answer = 0
+keepGoing = True
+while( keepGoing ):
+	if( check( value, 20 ) ):
+		answer = value
+		keepGoing = False
+	value += 1
+
+print answer
